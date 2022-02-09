6dfcae54199b5ff30dbe072dc6b07e9b379ff0e7
Wes Modes <wmodes@gmail.com>
2022-02-09 08:58:19 -0800

    Added dogs

---
+++ lab4/js/new.js
@@ -0,0 +1,7 @@
+// Print the board before each turn
+    // Loop through each row
+      // Loop through each space
+          // Print X, O, or Space
+          // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
