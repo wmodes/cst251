625401a4f661e30309de0b0f19b82285f4efc7ef
Wes Modes <wmodes@gmail.com>
2022-03-21 13:57:27 -0700

    Lab 14

---
+++ lab14/lab.js
@@ -0,0 +1,17 @@
+
+
+var recipes = [
+  {
+    title: "Yummy Spagetti",
+    link: "https://yummyeats.com/spagetti/",
+    // vegan: false,
+    // vegie: true,
+    // lactose: true,
+    tags: ["veg", "lacfree", "cheap"]
+  },
+  {
+    title: "Butternut Squash Bake",
+    link: "https://allfoooooods.com/recipes/463372382",
+    tags: ["vegan", "veg", "lacfree", "cheap"]
+  },
+]
